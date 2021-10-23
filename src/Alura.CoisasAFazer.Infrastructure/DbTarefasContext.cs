857d418053f2877a6c00a3c16e7e38310098fe55
Diego <diegomcarvalho7@gmail.com>
2021-10-22 21:58:26 -0300

    Projeto Aula 01

---
+++ src/Alura.CoisasAFazer.Infrastructure/DbTarefasContext.cs
@@ -0,0 +1,28 @@
+﻿using Alura.CoisasAFazer.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.CoisasAFazer.Infrastructure
+{
+    public class DbTarefasContext : DbContext
+    {
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        
+        public DbTarefasContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbTarefasContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = DbTarefas; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
